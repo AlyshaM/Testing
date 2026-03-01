aa049eeea636a3e5ab52ef26cd87bd94ee901ee7
Alysha McCullough <aly.mccullough01@gmail.com>
2026-02-28 21:51:36 -0800

    Hello world added

---
+++ MyApp/helloWorld.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This line prints "Hello, World!" to the console.
+            Console.WriteLine("Hello, World! :D");
+            // Wait for user input before closing the console window.
+            Console.ReadKey();
+        }
+    }
+}
